ad56c86e341ea1f45f1512ce4d6e23df060d46d0
JEFERSON FERREIRA DA SILVA <jeferson.ferreira@ifpa.local>
2015-10-27 16:50:40 -0300

    Implementado o Tipo de cliente PF e PJ.
    Tipos de clientes
    
    Agora que você já tem seu cadastro de cliente, você deverá também aceitar
    clientes Pessoa Jurídica em sua listagem.
    
    Não se esqueça de trabalhar com os modificadores de acesso e os getters e
    setters.
    
    No momento da listagem dos clientes, você deve indicar em uma das colunas
    se o cliente é pessoa física ou jurídica.
    
    Ambos os tipos devem implementar uma interface onde seja possível
    classificar o grau de importância do cliente para a empresa (ex: cliente
    1,2,3,5 estrelas).
    
    Você também terá a opção de criar clientes que utilizão endereço
    específico de cobrança, nesse caso, crie uma interface para que esses
    tipos de clientes possam implementar.

---
+++ module/Application/Model/IEnderecoResidencial.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeferson.ferreira
+ * Date: 27/10/2015
+ * Time: 15:06
+ */
---
+++ module/Application/Model/PessoaJuridica.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jeferson.ferreira
+ * Date: 27/10/2015
+ * Time: 14:13
+ */
+class PessoaJuridica
+{
+
+}
